06856dc66a37d782d3c69fc8e06fac9c9d55d0ee
qevanoraofficial <qevanoraofficial@gmail.com>
2026-08-16 13:26:20 +0700

    feat: add Spotify Prime Video and Redeem Nitro product images

--- src/lib/premium-app-images.ts
+++ src/lib/premium-app-images.ts
@@ -8,6 +8,18 @@ const PREMIUM_APP_IMAGES: Array<{ pattern: RegExp; src: string }> = [
     src: "/images/premium-apps/capcut-user.png",
   },
   {
+    pattern: /spotify|spootify/i,
+    src: "/images/premium-apps/spotify-user.png",
+  },
+  {
+    pattern: /prime\s*video|primevidio|prime\s*vidio/i,
+    src: "/images/premium-apps/prime-video-user.png",
+  },
+  {
+    pattern: /redeem\s*nitro|discord\s*nitro|nitro/i,
+    src: "/images/premium-apps/redeem-nitro-user.png",
+  },
+  {
     pattern: /\bviu\b/i,
     src: "/images/premium-apps/viu-user.png",
   },
